d481c42ba9e7dc81aa631e8665e2dd39704a1d64
ArefinAlter <adrita.stu2018@juniv.edu>
2025-08-15 19:35:47 +0600

    wokrs

--- index.php
+++ index.php
@@ -18,12 +18,7 @@ include 'partials/_header.php';
         <div class="hero__container">
             <div class="hero__content">
                 <div class="hero__text">
-                    <div class="hero__badge">
-                        <svg class="hero__badge-icon" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                            <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
-                        </svg>
-                        <span>10 Years Experience</span>
-                    </div>
+                    <!-- 10 Years Experience badge removed -->
                     <h1 class="hero__title">
                         Connecting <span class="hero__title-highlight">Global Brands</span> with Quality Manufacturers
                     </h1>
