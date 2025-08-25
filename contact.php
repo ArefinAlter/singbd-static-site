81118c35742c7b83a7da3e2c9c686d04eedc19d8
ArefinAlter <adrita.stu2018@juniv.edu>
2025-08-26 05:15:15 +0600

    product listing

--- contact.php
+++ contact.php
@@ -94,7 +94,7 @@ include 'partials/_header.php';
                         <iframe 
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.819123456789!2d103.8583!3d1.3000!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMcKwMTgnMDAuMCJOIDEwM8KwNTEnMjkuOSJF!5e0!3m2!1sen!2ssg!4v1234567890"
                             width="100%" 
-                            height="300" 
+                            height="500" 
                             style="border: 0;" 
                             allowfullscreen 
                             loading="lazy" 
